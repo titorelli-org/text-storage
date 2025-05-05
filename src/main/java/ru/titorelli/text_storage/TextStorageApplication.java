ede7e3d362d771676db34abee980a018661c8403
ychebotaev <yury.79120345101@gmail.com>
2025-05-05 23:37:00 +0500

    fix: remove @Configuration from app

--- src/main/java/ru/titorelli/text_storage/TextStorageApplication.java
+++ src/main/java/ru/titorelli/text_storage/TextStorageApplication.java
@@ -3,14 +3,12 @@ package ru.titorelli.text_storage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
-@Configuration
 @EnableWebSecurity
 public class TextStorageApplication {
 
