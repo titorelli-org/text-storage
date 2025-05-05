0538efaeb4afe40858cdc328ab023a997744318e
ychebotaev <yury.79120345101@gmail.com>
2025-05-05 23:15:38 +0500

    feat: add basic auth

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build . -t titorelli/text-storage:latest
--- src/main/java/ru/titorelli/text_storage/TextStorageApplication.java
+++ src/main/java/ru/titorelli/text_storage/TextStorageApplication.java
@@ -2,12 +2,27 @@ package ru.titorelli.text_storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
+@Configuration
+@EnableWebSecurity
 public class TextStorageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TextStorageApplication.class, args);
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+        return  httpSecurity.build();
+    }
 }
